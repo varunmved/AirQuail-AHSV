e0e735fbcd835615c559e20b85b70368ecc15df6
Varun Ved <varunmved@gmail.com>
2015-07-18 16:49:36 -0700

    nothing works holy shit

---
+++ Backend/index.js
@@ -0,0 +1,2 @@
+var cps = require('./node_modules/cps-api');
+var conn = new cps.Connection('tcp://cloud-us-0.clusterpoint.com:9007', 'AirQuail', 'varunmved', 'swag123', 'document', 'document/id', {account: 100898});
